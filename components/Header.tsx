98fe09745a4ae72bb11ee4fd333e01eea395b692
breezemRedSwitches <breezem@redswitches.com>
2024-10-07 16:25:42 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,49 @@
 import React from "react";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="bg-[#0a001d] text-white py-4">
+      <div className="container mx-auto flex justify-between items-center">
+       
+        <div className="flex items-center space-x-2">
+          <img
+            src="/branding/logo.svg"
+            alt="Talentino Logo"
+            className="h-8"
+          />
+          <span className="text-xl font-bold">talentino</span>
+        </div>
+
+      
+        <nav className="hidden md:flex space-x-8 text-white">
+          <a href="#" className="hover:text-gray-300">
+            Features
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            Pricing
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            About
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            Blog
+          </a>
+          <a href="#" className="hover:text-gray-300">
+            Contact
+          </a>
+        </nav>
+
+        <div>
+          <a
+            href="#"
+            className="border border-white px-4 py-2 hover:bg-white hover:text-black transition duration-300"
+          >
+            Get started
+          </a>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
