c0ad6c1b931cb428c5e789780765c7465cd7a185
APPLE MAC HUB <applemachub@APPLEs-MacBook-Pro.local>
2024-10-07 18:31:00 +0500

    Header & Footer added

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="">
+      <main className="">
         <p>This is Main Page</p>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <p>This is Footer</p>
-      </footer>
     </div>
   );
 }
